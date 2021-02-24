9906af4e3bd35304fb83b3138eb4145d8fedf223
to-yuki <to-yuki@jtp.co.jp>
2021-02-24 12:52:25 +0900

    bug fix
    ENV TestPHP

--- envtest.php
+++ envtest.php
@@ -3,9 +3,9 @@
 $myenv = getenv('MYMESSAGE');
 if (!empty($myenv)) {
   // 環境変数が空でなければ。
-  echo 'MYMESSAGE ENV : $myenv';
+  echo "MYMESSAGE ENV : ${myenv}";
 } else {
   // 環境変数が空ならば
-  echo 'MYMESSAGE ENV not Setting!';
+  echo "MYMESSAGE ENV not Setting!";
 }
 ?>
