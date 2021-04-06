df5e16b9d56715efc53b216903bbd86074034ccc
to-yuki <to-yuki@jtp.co.jp>
2021-04-06 16:29:02 +0900

    Update index.php
    ip

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>';
+   echo "Server IP address\n\n";
+   echo $_SERVER[ 'SERVER_ADDR' ];
+ ?> 
  </body>
 </html>
